31934479bf7508666cee98d2db82e6d0c42a579e
Reginald Cosens <ReginaldCosensIII@gmail.com>
2026-01-13 04:19:28 -0500

    feat: standardize recoverable error surface

--- apps/tool/src/index.tsx
+++ apps/tool/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ErrorBoundary } from './components/error/ErrorBoundary';
+import { ToastProvider } from './context/ToastContext';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ErrorBoundary>
-            <App />
+            <ToastProvider>
+                <App />
+            </ToastProvider>
         </ErrorBoundary>
     </React.StrictMode>,
 );
